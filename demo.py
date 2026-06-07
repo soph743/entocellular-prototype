4f23225f219943820a76d19a4d4a09f900506eda
Sophia Cherkaoui <sophiacherkaoui@Sophias-MacBook-Air-5.local>
2026-06-07 11:55:48 -0400

    modified demo file

--- demo.py
+++ demo.py
@@ -6,8 +6,12 @@ image = tifffile.imread('sample-data/Well_C2_Xmm0.987_Ymm-0.999_Ch1_1um.tiff')
 labels, count = cellcounter.label(image, method='mahotas')
 print(f"Found {count} cells using mahotas.")
 
-#cellcounter.display(image, labels, count)
+cellcounter.display(image, labels, count)
 
 labels, count = cellcounter.label(image, method='cellpose3')
 print(f"Found {count} cells using cellpose3.")
+cellcounter.display(image, labels, count)
+
+labels, count = cellcounter.label(image, method='cellpose3_tiled')
+print(f"Found {count} cells using cellpose3_tiled.")
 cellcounter.display(image, labels, count)
